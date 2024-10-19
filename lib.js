2ba69d4cf84b3b8cc45ddefd5cad620325ca45be
Mihai Moise <mihai.moise@uipath.com>
2024-10-20 01:46:06 +0300

    Update lib.js for C53

--- lib.js
+++ lib.js
@@ -1,10 +1,10 @@
 "use strict";
 
-const {SerialPort} = require("serialport");
-const {ReadlineParser} = require('@serialport/parser-readline');
+const { SerialPort } = require("serialport");
+const { ReadlineParser } = require('@serialport/parser-readline');
 let util = require("util"),
-events = require('events');
-const {usb} = require('usb');
+    events = require('events');
+const { usb } = require('usb');
 
 function McIntosh() {
     this.seq = 0;
@@ -25,14 +25,14 @@ let _processw = function () {
 
     this._port.write(this._qw[0] + "\n",
         (err) => {
-        if (err)
-            return;
-        this._qw.shift();
-        this._woutstanding = false;
-        setTimeout(() => {
-            _processw.call(this);
-        }, 150);
-    });
+            if (err)
+                return;
+            this._qw.shift();
+            this._woutstanding = false;
+            setTimeout(() => {
+                _processw.call(this);
+            }, 150);
+        });
 }
 
 function send(val, cb) {
@@ -41,43 +41,31 @@ function send(val, cb) {
 };
 
 McIntosh.prototype.volume_up = function () {
-    if (this.serialCommandMode == "Zone") {
-        send.call(this, "(VUP Z1)\n");
-    } else {
-        send.call(this, "(VOL U)\n");
-    }
+    send.call(this, "(VOL U)\n");
 };
 McIntosh.prototype.volume_down = function () {
-    if (this.serialCommandMode == "Zone") {
-        send.call(this, "(VDN Z1)\n");
-    } else {
-        send.call(this, "(VOL D)\n");
-    }
 
+    send.call(this, "(VOL D)\n");
 };
-McIntosh.prototype.set_volume = function (val) {
+
+McIntosh.prototype.set_volume = function (val, force = false) {
     if (this.properties.volume == val)
         return;
+    send.call(this, "(VOL " + val + ")\n");
+    return;
+    
     if (this.volumetimer)
         clearTimeout(this.volumetimer);
-    var self = this;
     this.volumetimer = setTimeout(() => {
-        if (self.serialCommandMode == "Zone") {
-            send.call(this, "(VST Z1 " + val + ")\n");
-        } else {
-            send.call(this, "(VOL " + val + ")\n");
-        }
+        send.call(this, "(VOL " + val + ")\n");
     }, 50)
 };
 McIntosh.prototype.get_status = function () {
     send.call(this, "(QRY)\n");
 };
 McIntosh.prototype.power_off = function () {
-    if (this.serialCommandMode == "Zone") {
-        send.call(this, "(POF Z1)\n");
-    } else {
-        send.call(this, "(PWR 0)\n");
-    }
+    send.call(this, "(PWR 0)\n");
+
     let val = "Standby";
     if (this.properties.source != val) {
         this.properties.source = val;
@@ -86,30 +74,17 @@ McIntosh.prototype.power_off = function () {
 
 };
 McIntosh.prototype.power_on = function () {
-    if (this.serialCommandMode == "Zone") {
-        send.call(this, "(PON Z1)\n");
-    } else {
-        send.call(this, "(PWR 1)\n");
-    }
-
+    send.call(this, "(PWR 1)\n");
 };
 McIntosh.prototype.set_source = function (val) {
-    if (this.serialCommandMode == "Zone") {
-        send.call(this, "(INP Z1 " + val + ")\n");
-    } else {
-        send.call(this, "(INP " + val + ")\n");
-    }
+    send.call(this, "(INP " + val + ")\n");
 };
 McIntosh.prototype.mute = function (val) {
-    if (this.serialCommandMode == "Zone") {
-        send.call(this, "(MUT Z1 " + val + ")\n");
-    } else {
-        send.call(this, "(MUT " + val + ")\n");
-    }
+    send.call(this, "(MUT " + val + ")\n");
 };
 
 McIntosh.prototype.raw_command = function (val) {
-        send.call(this, val + "\n");
+    send.call(this, val + "\n");
 };
 
 McIntosh.prototype.init = function (opts, closecb) {
@@ -125,7 +100,6 @@ McIntosh.prototype.init = function (opts, closecb) {
     };
 
     this.initializing = true;
-    this.serialCommandMode = "Zone";
 
     this._port = new SerialPort({
         path: opts.port,
@@ -188,14 +162,12 @@ McIntosh.prototype.init = function (opts, closecb) {
 
         } else if (/^.*\(PWR .*$/.test(data)) {
             //if PWR is received, device that is not using Zones is in use -> use 'NoZone' mode
-            this.serialCommandMode = "NoZone";
-
             if (/^.*\(PWR 0$/.test(data)) {
-              let val = "Standby";
-              if (this.properties.source != val) {
-                  this.properties.source = val;
-                  this.emit('source', val);
-              }
+                let val = "Standby";
+                if (this.properties.source != val) {
+                    this.properties.source = val;
+                    this.emit('source', val);
+                }
             }
         } else if (/^\(VOL ([0-9]*)$/.test(data)) {
             let val = Number(data.trim().replace(/^\(VOL ([0-9]*)$/, "$1"));
@@ -208,100 +180,92 @@ McIntosh.prototype.init = function (opts, closecb) {
             }
         }
     });
-        let timer = setTimeout(() => {
-            if (this.initializing) {
-                this.initializing = false;
-                this.emit('connected');
-            }
-        }, 3000);
-        this._port.on('open', err => {
-            this.emit('preconnected');
+    // let timer = setTimeout(() => {
+    //     if (this.initializing) {
+    //         this.initializing = false;
+    //         this.emit('connected');
+    //     }
+    // }, 3000);
+
+    this._port.on('open', err => {
+        let val = "Standby";
+        this.properties.source = val;
+        //get device status in case it's up
+        send.call(this, "(QRY)\n");
+    });
+
+    //detection of McIntosh USB disconnection (at power-off)
+    usb.on('detach', device => {
+        if (this.properties.usbVid == device.deviceDescriptor.idVendor) {
+            console.log('remove', device);
             let val = "Standby";
-            this.properties.source = val;
-            //get device status in case it's up
-            send.call(this, "(QRY)\n");
-            //get volume in case device is running (QRY does not report volume, so we need to use a 'trick')
-            send.call(this, "(VDN Z1)\n");
-            send.call(this, "(VUP Z1)\n");
-            send.call(this, "(VOL D)\n");
-            send.call(this, "(VOL U)\n");
-            //        send.call(this, "(PON Z1)\n");
-            //        send.call(this, "(INP Z1 " + this.properties.source + ")\n");
-            //        send.call(this, "(VST Z1 " + this.properties.volume + ")\n");
-        });
-
-        //detection of McIntosh USB disconnection (at power-off)
-        usb.on('detach', device => {
-            if (this.properties.usbVid == device.deviceDescriptor.idVendor) {
-                console.log('remove', device);
-                let val = "Standby";
-                if (this.properties.source != val) {
-                    this.properties.source = val;
-                    this.emit('source', val);
-                }
+            if (this.properties.source != val) {
+                this.properties.source = val;
+                this.emit('source', val);
             }
-        });
+        }
+    });
 
-        this._port.on('close', () => {
-            this._port.close(() => {
-                this._port = undefined;
-                if (closecb) {
-                    var cb2 = closecb;
-                    closecb = undefined;
-                    cb2('close');
-                }
-            })
-        });
-        this._port.on('error', err => {
-            this._port.close(() => {
-                this._port = undefined;
-                if (closecb) {
-                    var cb2 = closecb;
-                    closecb = undefined;
-                    cb2('error');
-                }
-            })
-        });
-        this._port.on('disconnect', () => {
-            this._port.close(() => {
-                this._port = undefined;
-                if (closecb) {
-                    var cb2 = closecb;
-                    closecb = undefined;
-                    cb2('disconnect');
-                }
-            })
-        });
-    };
-
-        McIntosh.prototype.start = function (opts) {
-        this.seq++;
-
-        let closecb = (why) => {
-            this.emit('disconnected');
-            if (why != 'close') {
-                var seq = ++this.seq;
-                setTimeout(() => {
-                    if (seq != this.seq)
-                        return;
-                    this.start(opts);
-                }, 1000);
+    this._port.on('close', () => {
+        this._port.close(() => {
+            this._port = undefined;
+            if (closecb) {
+                var cb2 = closecb;
+                closecb = undefined;
+                cb2('close');
             }
-        };
+        })
+    });
+    this._port.on('error', err => {
+        this._port.close(() => {
+            this._port = undefined;
+            if (closecb) {
+                var cb2 = closecb;
+                closecb = undefined;
+                cb2('error');
+            }
+        })
+    });
+    this._port.on('disconnect', () => {
+        this._port.close(() => {
+            this._port = undefined;
+            if (closecb) {
+                var cb2 = closecb;
+                closecb = undefined;
+                cb2('disconnect');
+            }
+        })
+    });
+};
 
-        if (this._port) {
-            this._port.close(() => {
-                this.init(opts, closecb);
-            });
-        } else {
-            this.init(opts, closecb);
+McIntosh.prototype.start = function (opts) {
+    this.seq++;
+
+    let closecb = (why) => {
+        this.emit('disconnected');
+        if (why != 'close') {
+            var seq = ++this.seq;
+            setTimeout(() => {
+                if (seq != this.seq)
+                    return;
+                this.start(opts);
+            }, 1000);
         }
     };
 
-        McIntosh.prototype.stop = function () {
-        this.seq++;
-        if (this._port)
-            this._port.close(() => {});
-    };
+    if (this._port) {
+        this._port.close(() => {
+            this.init(opts, closecb);
+        });
+    } else {
+        this.init(opts, closecb);
+    }
+};
 
-        exports = module.exports = McIntosh;
+McIntosh.prototype.stop = function () {
+    this.seq++;
+    if (this._port)
+        this._port.close(() => { });
+};
+
+exports = module.exports = McIntosh;
